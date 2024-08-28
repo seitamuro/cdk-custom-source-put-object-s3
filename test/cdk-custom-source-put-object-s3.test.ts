6a35e8c76b012db73984c5012b718b919e482e09
seitamuro <seitamuro0210@gmail.com>
2024-08-29 08:38:02 +0900

    Initial commit

---
+++ test/cdk-custom-source-put-object-s3.test.ts
@@ -0,0 +1,17 @@
+// import * as cdk from 'aws-cdk-lib';
+// import { Template } from 'aws-cdk-lib/assertions';
+// import * as CdkCustomSourcePutObjectS3 from '../lib/cdk-custom-source-put-object-s3-stack';
+
+// example test. To run these tests, uncomment this file along with the
+// example resource in lib/cdk-custom-source-put-object-s3-stack.ts
+test('SQS Queue Created', () => {
+//   const app = new cdk.App();
+//     // WHEN
+//   const stack = new CdkCustomSourcePutObjectS3.CdkCustomSourcePutObjectS3Stack(app, 'MyTestStack');
+//     // THEN
+//   const template = Template.fromStack(stack);
+
+//   template.hasResourceProperties('AWS::SQS::Queue', {
+//     VisibilityTimeout: 300
+//   });
+});
